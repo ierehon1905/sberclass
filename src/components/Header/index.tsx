0d22d55cc258a737f1d43dc281cdb25ce485688b
Klim Falkenberg <mail@klim.me>
2020-10-17 05:53:09 +0300

    feat: — header styles — logo, sublogo, slash

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,39 @@
 import React from "react";
 import styled from "styled-components";
+import Logo from '../images/Logo.svg'
+import Slash from '../images/Slash.svg'
+import SmartFlowLogo from '../images/SmartFlowLogo.svg'
 
-const StyledHeader = styled.header``;
+const StyledHeader = styled.header`
+  height: 60px;
+  background: #FFFFFF;
+  box-shadow: 0px 4px 28px rgba(52, 58, 69, 0.1);
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
+  .logo {
+    width: auto;
+    height: 28px
+  }
+  .slash {
+    width: auto;
+    height: 28px
+
+  }
+  .sub-logo {
+    width: auto;
+    height: 18px
+
+  }
+  img {
+    margin-right: 10px;
+  }
+`;
 
 export const Header: React.FC<any> = props => (
   <StyledHeader {...props}>
-    <h1>Сбер Класс</h1>
+        <img className='logo' src={Logo} alt="" />
+        <img className='slash' src={Slash} alt="" />
+        <img className='sub-logo' src={SmartFlowLogo} alt="" />
   </StyledHeader>
 );
--- src/containers/NavBar/index.tsx
+++ src/containers/NavBar/index.tsx
@@ -1,10 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
-import editScr from "./Edit.svg";
-import settingsSrc from "./Settings.svg";
-import gridSrc from "./Grid.svg";
-import listSrc from "./List.svg";
 import Icon from "../../components/Icon";
 import { colors } from "../../utils/colors";
 
